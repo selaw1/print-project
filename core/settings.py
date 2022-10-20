9f4858be41ba0f357e75e5c8269f62ba3622cbc1
Yousef <usefselawi@gmail.com>
2022-10-20 11:04:06 +0400

    dashboard app / UI improvements

--- core/settings.py
+++ core/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # My apps
     'accounts',
-    'common'
+    'common',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 from accounts.urls import translatable_urlpatterns as accounts_urls
+from dashboard.urls import translatable_urlpatterns as dashboard_urls
 
 
 urlpatterns = [
@@ -9,3 +10,4 @@ urlpatterns = [
 ]
 
 urlpatterns += accounts_urls
+urlpatterns += dashboard_urls
